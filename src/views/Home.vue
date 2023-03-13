1ef725e01ff4c892672c0c7ecb6864d3a5596a52
Killian Boisseau <killianboisseau85@gmail.com>
2023-03-13 16:26:40 -0600

    changement ip webservice

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -98,7 +98,7 @@ export default {
     };
   },
   mounted() {
-    fetch("http://localhost:8000/api/lugares")
+    fetch("https://web-service-380302.uc.r.appspot.com/api/lugares")
       .then((res) => res.json())
       .then((data) => (this.lugares = data))
       .catch((err) => console.log(err.message));
